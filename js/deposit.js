43b1830d9504e54f7ec2a9381873e7b08ea971be
Unumongmarmahub <unumongmarma1998@gmail.com>
2022-08-17 20:55:32 +0600

    project compleate

---
+++ js/deposit.js
@@ -0,0 +1,31 @@
+// Step-1 add event listener to deposit button
+
+document.getElementById('btn_dposit').addEventListener('click', function () {
+  // Step-2 get the deposit amount from the deposit field 
+  // for input field use.value to the value inside input field
+
+  const depoistField = document.getElementById('deposit-field');
+  const newDepositAmountString = depoistField.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
+  //  Step-3 get to current deposit total amount
+  // for non input (element other input textarea), use inner text get the text  
+
+  const depositTotalElement = document.getElementById('deposit_total');
+  const previousDepositTotaString = depositTotalElement.innerText;
+  const previousDepositTotal = parseFloat(previousDepositTotaString);
+  // Step-4 add numbers to set the total deposit 
+  const currentDepositTotal = previousDepositTotal + newDepositAmount;
+  // set the deposit total 
+  depositTotalElement.innerText = currentDepositTotal;
+  // Step-5 get ballance current total
+  const ballanceTotalElement = document.getElementById('balance_total');
+  const previousBalanceTotalString = ballanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+  // Step-6 calculate current total balance 
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  // set the balance total 
+  ballanceTotalElement.innerText = currentBalanceTotal;
+
+  // Step-7 clear the deposit feild 
+  depoistField.value = '';
+})
---
+++ js/withdraw.js
@@ -0,0 +1,40 @@
+/*
+ 
+1. add event handler with the withdraw btn
+2. get the withdraw amount form the withdraw input field
+2-5 also make sure to convert the input into a number by using parseFolt
+3. get previous withdraw total 
+4. calculate total withdraw amount
+4-5. set total withdraw amount 
+5. get the previous balance total 
+6. calculate new balnce total
+6-5. set the new balance total 
+7. clear the input feild
+*/
+// Step 1
+document.getElementById('btn_withdraw').addEventListener('click', function () {
+  // Step 2
+  const withdrawField = document.getElementById('withdraw_field');
+  const newWithdrawAmountString = withdrawField.value;
+  const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+  // Step 3
+  const WithdrawTotalElement = document.getElementById('withdraw_total');
+  const previousWithdrawTotalString = WithdrawTotalElement.innerText;
+  const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+  // Step 4
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  WithdrawTotalElement.innerText = currentWithdrawTotal;
+  // Step 5 
+  const ballanceTotalElement = document.getElementById('balance_total');
+  const previousBalanceTotalString = ballanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  // Step 6
+  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+  ballanceTotalElement.innerText = newBalanceTotal;
+
+  // Step 7
+  withdrawField.value = '';
+
+})
