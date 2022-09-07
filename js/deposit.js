94ddc02c57aabb8b77f0b3a8f4959742c7ca3744
Unumongmarmahub <unumongmarma1998@gmail.com>
2022-09-07 15:11:04 +0600

    another solve

--- js/deposit.js
+++ js/deposit.js
@@ -1,14 +1,13 @@
-// Step-1 add event listener to deposit button
+// Step-1: add event listener to deposit button
 
 document.getElementById('btn_dposit').addEventListener('click', function () {
-  // Step-2 get the deposit amount from the deposit field 
-  // for input field use.value to the value inside input field
-
+  // Step-2: get the deposit amount from the deposit input field 
+  // for input field use (dot).value to the value inside input field
   const depoistField = document.getElementById('deposit-field');
   const newDepositAmountString = depoistField.value;
   const newDepositAmount = parseFloat(newDepositAmountString);
-  //  Step-3 get to current deposit total amount
-  // for non input (element other input textarea), use inner text get the text  
+  //  Step-3: get to current deposit total amount
+  // for non input (element other than input textarea), use inner text get the text  
 
   // Step-7 clear the deposit feild 
   depoistField.value = '';
@@ -23,16 +22,19 @@ document.getElementById('btn_dposit').addEventListener('click', function () {
   const previousDepositTotal = parseFloat(previousDepositTotaString);
   // Step-4 add numbers to set the total deposit 
   const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
   // set the deposit total 
   depositTotalElement.innerText = currentDepositTotal;
+
   // Step-5 get ballance current total
   const ballanceTotalElement = document.getElementById('balance_total');
   const previousBalanceTotalString = ballanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
   // Step-6 calculate current total balance 
   const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  
   // set the balance total 
   ballanceTotalElement.innerText = currentBalanceTotal;
-
 
 })
--- js/login.js
+++ js/login.js
@@ -1,16 +1,19 @@
 
 // step: 1 add click event handler with the submit button 
 document.getElementById('btn_submit').addEventListener('click', function () {
+
   // step: 2 get the email address inside the email input feild 
   // always remamber to use (dot).value to get text form an input feild 
   const emailFild = document.getElementById('user_email');
   const email = emailFild.value;
+
   // step: 3 get passward 
   // 3.a: set id on the html element 
   // 3.b: get the element 
   // 3.c: get the value from the element 
   const passwarField = document.getElementById('user_passward');
   const passward = passwarField.value;
+  
   // Danger: DO NOT VERYFY email passward on the clint side
   // step:4 veryfy email and passward and check whether valid user or not 
   if (email === 'sontan@gmail.com' && passward === 'secret') {
